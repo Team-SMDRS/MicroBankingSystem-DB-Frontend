1ad192f382aa9eebda1bab4c93727001d6c80cca
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-08-25 21:24:21 +0530

    feat:add tailwind CSS

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-blue-900 text-orange-300 text-3xl font-bold">
+      🚀 Hellow sangeeth
+    </div>
+  )
+}
+
+export default App;
